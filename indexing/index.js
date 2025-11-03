1b2a1dac6454fbd6656eb70862c3173fc62e66c4
yash766786 <yashchoudhary766786@gmail.com>
2025-11-04 02:33:34 +0530

    adding workflow and indexing in RAG

---
+++ indexing/index.js
@@ -0,0 +1,42 @@
+// this code is only for how indexing done in RAG
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';  
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'; 
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from '@langchain/pinecone';
+
+async function indexDocument() {   
+    // pdf loading
+    const PDF_PATH = './dsa2.pdf';
+    const pdfLoader = new PDFLoader(PDF_PATH);
+    const rawDocs = await pdfLoader.load();
+
+    // chunking
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+    const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
+
+    // vector embedding model
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+        apiKey: process.env.GEMINI_API_KEY,
+        model: 'text-embedding-004',
+    });
+
+    // configure Database
+    // Initialize Pinecone Client
+    const pinecone = new Pinecone();
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+    // langchain (chunking, embedding, database)
+    await PineconeStore.fromDocuments(chunkedDocs, embeddings, {
+        pineconeIndex,
+        maxConcurrency: 5,
+    });
+}
+
+indexDocument();
